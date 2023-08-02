d759ebf8a5e17716d954c7dcd655159ba9ed62e2
DylanPiriou <dylan.piriou@hotmail.com>
2023-08-02 20:25:28 +0200

    h1 speed edited and custom cursor

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ gsap.to(square, {
         trigger: section,
         scrub: true,
         pin: true,
-        end: "bottom 50%"
+        start: "top top",
+        end: "bottom 50%",
+        ease: "linear"
     },
     width: "100%",
     height: "101%",
@@ -33,9 +35,39 @@ gsap.to(title, {
     scrollTrigger: {
         trigger: section,
         scrub: true,
-        start: "top top",
-        end: "bottom 50%"
+        start: "top 5%",
+        end: "bottom 70%"
     },
-    y: "-50%"
+    y: "-50%",
+    ease: "linear"
 });
 
+const h1 = document.querySelector("h1");
+const cursor = document.querySelector(".cursor")
+
+h1.addEventListener("mouseover", () => {
+    gsap.to(cursor, {
+        width: 400,
+        height: 400,
+        ease: Power2.ease
+    })
+})
+
+h1.addEventListener("mouseleave", () => {
+    gsap.to(cursor, {
+        width: 40,
+        height: 40
+    })
+})
+
+window.addEventListener("scroll", () => {
+    const scrollAmount = window.scrollY;
+    const newPosition = scrollAmount * .3;
+    h1.style.transform = `translateY(${newPosition}px)`;
+
+})
+
+window.addEventListener("mousemove", e => {
+    let { pageX, pageY } = e;
+    gsap.set(cursor,{x :pageX - cursor.getBoundingClientRect().width / 2 ,y :pageY - cursor.getBoundingClientRect().width / 2})
+})
